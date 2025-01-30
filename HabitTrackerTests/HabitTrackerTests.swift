f035f2188ae07404c2323eb7f6f98f1942029fa7
nex77qoz <nex77qoz@gmail.com>
2025-01-30 17:14:40 +0300

    Added snapshot testing

--- HabitTrackerTests/HabitTrackerTests.swift
+++ HabitTrackerTests/HabitTrackerTests.swift
@@ -1,36 +1,34 @@
-//
-//  HabitTrackerTests.swift
-//  HabitTrackerTests
-//
-//  Created by Максим Бабкин on 01.12.2024.
-//
-
 import XCTest
+import SnapshotTesting
 @testable import HabitTracker
 
-final class HabitTrackerTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+final class TrackerViewControllerSnapshotTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        isRecording = false
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testTrackerViewController_Light() {
+        let vc = TrackerViewController()
+        
+        vc.overrideUserInterfaceStyle = .light
+        
+        assertSnapshot(
+            matching: vc,
+            as: .image(on: .iPhone13Pro)
+        )
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testTrackerViewController_Dark() {
+        let vc = TrackerViewController()
+        
+        vc.overrideUserInterfaceStyle = .dark
+        
+        assertSnapshot(
+            matching: vc,
+            as: .image(on: .iPhone13Pro)
+        )
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
